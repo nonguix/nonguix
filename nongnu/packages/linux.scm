c1091e600a6c39d6efc79fe33c09dfe610d69450
Alex Griffin <a@ajgrf.com>
2019-05-27 16:54:30 -0500

    nongnu: Add linux@4.9.
    * nongnu/packages/linux.scm (linux-4.9): New variable.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -62,6 +62,10 @@ on hardware which requires nonfree software to function.")))
   (corrupt-linux linux-libre-4.14 "4.14.122"
                  "1xb87zwnw0sv9v53c61jlw9d1dl15m7rbq4h50yqpqbpz07g98y8"))
 
+(define-public linux-4.9
+  (corrupt-linux linux-libre-4.9 "4.9.179"
+                 "1qqy2ysd61gq9zlh1yg71354wn8rbp8hkz94j6nnv00305xlnbjk"))
+
 (define-public linux-firmware
   (let ((commit "92e17d0dd2437140fab044ae62baf69b35d7d1fa")
         (revision "1"))
