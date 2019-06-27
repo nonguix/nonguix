0edb327cf6d18889566202ac6babbc459d8ee8e1
Alex Griffin <a@ajgrf.com>
2019-06-27 09:36:02 -0500

    nongnu: linux@4.14: Update to 4.14.131.
    * nongnu/packages/linux.scm (linux-4.14): Update to 4.14.131.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -56,8 +56,8 @@ on hardware which requires nonfree software to function.")))
                  "1pvbjsy2bd76mmxxv6bf8kh7g6ixlrbj92cm5qq6yjdr2lr989kk"))
 
 (define-public linux-4.14
-  (corrupt-linux linux-libre-4.14 "4.14.130"
-                 "0najfj0m8s61hjyag4ll6bvv1y4d03dbgm137fmvfq4szx891ji9"))
+  (corrupt-linux linux-libre-4.14 "4.14.131"
+                 "1xcg7ij3r6271qmcymaxprqa8qvdddkh5f8m6gza3agl61641xhr"))
 
 (define-public linux-4.9
   (corrupt-linux linux-libre-4.9 "4.9.184"
