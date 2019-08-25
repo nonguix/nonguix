8fe311f5f5ff4f9d699717b304592700c8a5d7b4
Alex Griffin <a@ajgrf.com>
2019-08-25 09:52:53 -0500

    nongnu: linux: Update to 5.2.10.
    * nongnu/packages/linux.scm (linux): Update to 5.2.10.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -48,8 +48,8 @@
 on hardware which requires nonfree software to function.")))
 
 (define-public linux
-  (corrupt-linux linux-libre "5.2.9"
-                 "1rnlnphw9rih4qhdld9ic5lnj5jh4vy5nqbj9lqwv9bc615jmw5n"))
+  (corrupt-linux linux-libre "5.2.10"
+                 "0jgw7gj71i9kf4prbdi9h791ngxf24nr90302glnsa9aghwc95k0"))
 
 (define-public linux-4.19
   (corrupt-linux linux-libre-4.19 "4.19.68"
