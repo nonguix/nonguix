acffd4f7f2f48f0aaf9c9cc5f902bc24d9647f2d
Alex Griffin <a@ajgrf.com>
2019-12-14 13:25:43 -0600

    nongnu: Remove htmlcxx.
    It's packaged in GNU Guix now.  Closes #3.
    
    * nongnu/packages/gog.scm (htmlcxx): Remove variable.

--- nongnu/packages/gog.scm
+++ nongnu/packages/gog.scm
@@ -23,10 +23,8 @@
   #:use-module (gnu packages web)
   #:use-module (gnu packages xml)
   #:use-module (guix packages)
-  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
-  #:use-module (guix build-system gnu)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public lgogdownloader
@@ -68,40 +66,3 @@
     (description "LGOGDownloader is a client for the GOG.com download API,
 allowing simple downloads and updates of games and other files from GOG.com.")
     (license license:wtfpl2)))
-
-(define-public htmlcxx
-  (package
-    (name "htmlcxx")
-    (version "0.87")
-    (source
-     (origin
-       (method url-fetch)
-       (uri
-        (string-append "mirror://sourceforge/htmlcxx/v"
-                       version "/htmlcxx-" version ".tar.gz"))
-       (sha256
-        (base32 "1j3mzjlczjrk4ahc43s6kzpvzypzjmqz4sillnca5yadrwwgjf2x"))))
-    (build-system gnu-build-system)
-    (home-page "http://htmlcxx.sourceforge.net/")
-    (synopsis "Simple non-validating CSS1 and HTML parser for C++")
-    (description "htmlcxx is a simple non-validating CSS1 and HTML parser for
-C++.  Although there are several other HTML parsers available, htmlcxx has some
-characteristics that make it unique:
-@itemize
-@item STL like navigation of DOM tree, using excelent's tree.hh library from
-Kasper Peeters
-@item It is possible to reproduce exactly, character by character, the original
-document from the parse tree
-@item Bundled CSS parser
-@item Optional parsing of attributes
-@item C++ code that looks like C++ (not so true anymore)
-@item Offsets of tags/elements in the original document are stored in the nodes
-of the DOM tree
-@end itemize
-The parsing politics of htmlcxx were created trying to mimic Mozilla Firefox
-(https://www.mozilla.org) behavior.  So you should expect parse trees similar
-to those create by Firefox.  However, differently from Firefox, htmlcxx does
-not insert non-existent stuff in your html.  Therefore, serializing the DOM
-tree gives exactly the same bytes contained in the original HTML document.")
-    (license (list license:lgpl2.0
-                   license:asl2.0))))
