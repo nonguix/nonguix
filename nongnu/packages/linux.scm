1a7fdfe34d56bcfe1378d4ba8c7910af90d2451a
Alex Griffin <a@ajgrf.com>
2020-01-05 12:46:35 -0600

    nongnu: iwlwifi-firmware: Use gnu-build-system.
    * nongnu/packages/linux.scm (iwlwifi-firmware): Use gnu-build-system.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -180,28 +180,21 @@ is the Linux Bluetooth driver for Atheros AR3011/AR3012 Bluetooth chipsets.")
   (package
     (inherit linux-firmware)
     (name "iwlwifi-firmware")
-    (build-system trivial-build-system)
+    (build-system gnu-build-system)
     (arguments
-     `(#:modules ((guix build utils))
-       #:builder
-       (begin
-         (use-modules (guix build utils))
-         (let ((source (assoc-ref %build-inputs "source"))
-               (fw-dir (string-append %output "/lib/firmware/"))
-               (gzip (assoc-ref %build-inputs "gzip"))
-               (tar (assoc-ref %build-inputs "tar")))
-           (set-path-environment-variable "PATH" '("bin")
-                                          (list tar gzip))
-           (invoke "tar" "--strip-components=1" "-xvf" source)
-           (mkdir-p fw-dir)
-           (for-each (lambda (file)
-                       (copy-file file
-                                  (string-append fw-dir (basename file))))
-                     (cons*
-                      "./LICENCE.iwlwifi_firmware"
-                      (find-files "."
-                                  "iwlwifi-.*\\.ucode$")))
-           #t))))
+     `(#:tests? #f
+       #:license-file-regexp "LICENCE.iwlwifi_firmware"
+       #:phases
+       (modify-phases %standard-phases
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (fw-dir (string-append out "/lib/firmware")))
+               (for-each (lambda (file)
+                           (install-file file fw-dir))
+                         (find-files "." "iwlwifi-.*\\.ucode$"))
+               #t)))
+         (delete 'validate-runpath))))
     (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
     (synopsis "Nonfree firmware for Intel wifi chips")
     (description "The proprietary iwlwifi kernel module is required by many
