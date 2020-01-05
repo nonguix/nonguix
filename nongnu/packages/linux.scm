9c70f5144670c878ed701f27bf7b7c251419914b
Alex Griffin <a@ajgrf.com>
2020-01-05 13:08:18 -0600

    nongnu: rtl-nic-firmware: Use gnu-build-system.
    * nongnu/packages/linux.scm (rtl-nic-firmware): Use gnu-build-system.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -455,24 +455,21 @@ firmware for the following chips:
   (package
     (inherit linux-firmware)
     (name "rtl-nic-firmware")
-    (build-system trivial-build-system)
+    (build-system gnu-build-system)
     (arguments
-     `(#:modules ((guix build utils))
-       #:builder
-       (begin
-         (use-modules (guix build utils))
-         (let* ((source (assoc-ref %build-inputs "source"))
-                (fw-dir (string-append %output "/lib/firmware/"))
-                (bin-dir (string-append fw-dir "/rtl_nic"))
-                (gzip (assoc-ref %build-inputs "gzip"))
-                (tar (assoc-ref %build-inputs "tar")))
-           (set-path-environment-variable "PATH" '("bin")
-                                          (list tar gzip))
-           (invoke "tar" "--strip-components=1" "-xvf" source)
-           (mkdir-p bin-dir)
-           (copy-recursively "./rtl_nic" bin-dir)
-           (install-file "./LICENCE.rtlwifi_firmware.txt" fw-dir)
-           #t))))
+     `(#:tests? #f
+       #:license-file-regexp "LICENCE.rtlwifi_firmware.txt"
+       #:phases
+       (modify-phases %standard-phases
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (fw-dir (string-append out "/lib/firmware"))
+                    (bin-dir (string-append fw-dir "/rtl_nic")))
+               (mkdir-p bin-dir)
+               (copy-recursively "./rtl_nic" bin-dir)
+               #t)))
+         (delete 'validate-runpath))))
     (synopsis "Nonfree firmware for Realtek ethernet chips")
     (description
      "This package contains nonfree firmware for the following Realtek ethernet chips:
