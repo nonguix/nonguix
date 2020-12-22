6ae7f62be8ca9d25775e36ec0f43e6a58ece5e7e
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2020-12-22 16:57:21 +0100

    nongnu: firefox: Update to 84.0.1.
    * nongnu/packages/mozilla.scm (firefox): Update to 84.0.1.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -102,19 +102,19 @@
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20201215000000")
+(define %firefox-build-id "20201222000000")
 
 (define-public firefox
   (package
     (name "firefox")
-    (version "84.0")
+    (version "84.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "0f4iyvakql5pjf6nwllv5hl7106kaiw8xhq815fky92v2vq3w9r3"))))
+        (base32 "0hr2b5krk8wfraxhkh54c3rc3skbgaxd9yvl0shzk6d1f3900mdf"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags
