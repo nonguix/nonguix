128cbfc57e3e0b975cfd9947f97cd8bcf941d126
Cees de Groot <casedeg@gmail.com>
2021-03-27 10:30:02 -0400

    nongnu: Add dotnet-core-3.1.
    * nongnu/packages/dotnet.scm (dotnet-core-3.1): New variable.
    
    Signed-off-by: Jelle Licht <jlicht@fsfe.org>

--- nongnu/packages/dotnet.scm
+++ nongnu/packages/dotnet.scm
@@ -191,3 +191,113 @@ various IDEs and plugins.")
       (description "@code{.NET} is a cross-platform developer platform for
 building different types of applications.")
       (license license:expat))))
+
+(define-public dotnet-core-3.1
+  (let ((dotnet-sdk-version "3.1.13"))
+    (package
+      (name "dotnet")
+      (version "3.1.113")
+      (source
+       (origin
+         (method url-fetch/tarbomb)
+         (uri
+          (string-append "https://dotnetcli.azureedge.net/dotnet/Sdk/"
+                         version "/dotnet-sdk-"
+                         version "-linux-x64.tar.gz"))
+         (sha256
+          (base32
+           "0dm4kxpq235yfxzf41m1iyfg5avlxdic0nwr865g5wyzc6kz9nhw"))))
+      (build-system binary-build-system)
+      (arguments
+       `(#:patchelf-plan
+         `(("dotnet"
+            ("glibc" "gcc:lib" "zlib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version
+                             "/System.Net.Security.Native.so")
+            ("mit-krb5"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version
+                             "/System.Security.Cryptography.Native.OpenSsl.so")
+            ("openssl"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version
+                             "/System.IO.Compression.Native.so")
+            ("zlib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version
+                             "/libcoreclrtraceptprovider.so")
+            ("gcc:lib" "lttng-ust"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/createdump")
+            ("gcc:lib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libclrjit.so")
+            ("gcc:lib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libcoreclr.so")
+            ("gcc:lib" "icu4c"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libdbgshim.so")
+            ("gcc:lib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libhostpolicy.so")
+            ("gcc:lib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libmscordaccore.so")
+            ("gcc:lib"))
+           (,,(string-append "shared/Microsoft.NETCore.App/"
+                             dotnet-sdk-version "/libmscordbi.so")
+            ("gcc:lib"))
+           (,,(string-append "packs/Microsoft.NETCore.App.Host.linux-x64/"
+                             dotnet-sdk-version
+                             "/runtimes/linux-x64/native/apphost")
+            ("gcc:lib"))
+           (,,(string-append "packs/Microsoft.NETCore.App.Host.linux-x64/"
+                             dotnet-sdk-version
+                             "/runtimes/linux-x64/native/libnethost.so")
+            ("gcc:lib"))
+           (,,(string-append "sdk/" version "/AppHostTemplate/apphost")
+            ("gcc:lib"))
+           (,,(string-append "host/fxr/" dotnet-sdk-version "/libhostfxr.so")
+            ("gcc:lib")))
+         #:install-plan
+         `(("." "share/dotnet/"))
+         #:phases
+         (modify-phases %standard-phases
+           (add-before 'patchelf 'patchelf-writable
+             (lambda _
+               (for-each make-file-writable (find-files "."))))
+           (add-after 'install 'install-wrapper
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let* ((out (assoc-ref outputs "out"))
+                      (bin-dir (string-append out "/bin"))
+                      (dotnet-target (string-append out "/share/dotnet/dotnet"))
+                      (dotnet-dest (string-append bin-dir "/dotnet")))
+                 (mkdir-p bin-dir)
+                 (symlink dotnet-target dotnet-dest)
+                 ;; First symlink, then wrap-program: dotnet cannot run when renamed
+                 (wrap-program dotnet-dest
+                   ;; Ensure the `dotnet' program does not phone home to share telemetry and get
+                   ;; rid of a bunch of i18n warnings.
+                   `("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT" = ("1"))
+                   `("DOTNET_CLI_TELEMETRY_OPTOUT" = ("1")))))))))
+      (native-search-paths
+       (list (search-path-specification
+              (variable "DOTNET_ROOT")
+              (separator #f)
+              (files '("share/dotnet")))))
+      (inputs
+       `(("gcc:lib" ,gcc "lib")
+         ("glibc", glibc)
+         ("icu4c" ,icu4c)
+         ("lttng-ust" ,lttng-ust)
+         ("mit-krb5" ,mit-krb5)
+         ("openssl" ,openssl)
+         ("zlib" ,zlib)))
+      (home-page "https://docs.microsoft.com/en-us/dotnet/")
+      (supported-systems '("x86_64-linux"))
+      (synopsis "Binary build of the @code{.NET} SDK and runtime")
+      (description "@code{.NET} is a cross-platform developer platform for
+building different types of applications.")
+      (license license:expat))))
