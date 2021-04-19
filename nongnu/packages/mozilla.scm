c504f26622eaa093b958cd0dd02641ce54ca9f3e
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2021-04-19 23:52:45 +0200

    nongnu: firefox: Update to 88.0 [security fixes].
    Fixes CVE-2021-23994, CVE-2021-23995, CVE-2021-23996, CVE-2021-23997,
    CVE-2021-23998, CVE-2021-23999, CVE-2021-24000, CVE-2021-24001,
    CVE-2021-24002, CVE-2021-29945, CVE-2021-29944, CVE-2021-29946 and
    CVE-2021-29947.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 88.0.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -78,19 +78,19 @@
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20210323000000")
+(define %firefox-build-id "20210419000000")
 
 (define-public firefox
   (package
     (name "firefox")
-    (version "87.0")
+    (version "88.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "1asqzpvllsa6dlpaxclaykvrrwfanb23iir21yavcwgr482vx66f"))))
+        (base32 "15z2xsa402clh89rb3b2lysl9zgpv2hnafg20523x11z77xxnl3b"))))
     (build-system gnu-build-system)
     (arguments
      `(#:configure-flags
