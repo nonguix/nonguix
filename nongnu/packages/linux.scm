289a73904213238d1a69e34679907dbaea40a06e
pineapples <guixuser6392@protonmail.com>
2021-08-05 12:59:06 +0200

    nongnu: linux@5.13: Update to 5.13.8.
    * nongnu/packages/linux.scm (linux-5.13): Update to 5.13.8.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -58,8 +58,8 @@
 System on hardware which requires nonfree software to function.")))
 
 (define-public linux-5.13
-  (corrupt-linux linux-libre-5.13 "5.13.6"
-                 "0xjjl8dmilp425b1cp977v26qxlg1147gh54kni949pzxwh1fb56"))
+  (corrupt-linux linux-libre-5.13 "5.13.8"
+                 "0a54r71mcyqvq7c8kff817vh6zfj0bdzmp6ql4az84wq9nwc526h"))
 
 (define-public linux-5.10
   (corrupt-linux linux-libre-5.10 "5.10.54"
