01608562fbe2f97cf64a6cc9ecaece93b0ac90df
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2021-09-08 22:18:31 +0200

    nongnu: firefox: Rename icu4c-69 to icu4c.
    * nongnu/packages/mozilla.scm (firefox)[inputs]: Rename icu4c-69 to
    icu4c. On core-updates icu4c was updated to version 69.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -360,7 +360,7 @@
        ("gtk+" ,gtk+)
        ("gtk+-2" ,gtk+-2)
        ("hunspell" ,hunspell)
-       ("icu4c" ,icu4c-69)
+       ("icu4c" ,icu4c)
        ("jemalloc" ,jemalloc)
        ("libcanberra" ,libcanberra)
        ("libevent" ,libevent)
