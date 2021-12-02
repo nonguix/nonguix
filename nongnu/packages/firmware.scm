a57251eb84532872c2ecfbacb475cbe7038fcc9f
Petr Hodina <phodina@protonmail.com>
2021-12-02 13:48:19 +0100

    nongnu: Add ov5640-firmware.
    * nongnu/packages/firmware.scm (ov5640-firmware): New variable.
    
    Co-authored-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/firmware.scm
+++ nongnu/packages/firmware.scm
@@ -1,13 +1,16 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;;; Copyright © 2022 Jonathan Brielmaier <jonathan.brielmaier@web.de>
+;;; Copyright © 2022-2023 Jonathan Brielmaier <jonathan.brielmaier@web.de>
 ;;; Copyright © 2022 Petr Hodina <phodina@protonmail.com>
 
 (define-module (nongnu packages firmware)
   #:use-module (gnu packages efi)
   #:use-module (gnu packages firmware)
+  #:use-module (guix build-system copy)
   #:use-module (guix gexp)
+  #:use-module (guix git-download)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (nonguix licenses))
 
 ;; fwupd with LVFS nonfree repositories enabled
 (define-public fwupd-nonfree
@@ -29,3 +32,27 @@
                 (string-append "--libexecdir="
                                #$output "/libexec")
                 "-Dsupported_build=true"))))))
+
+(define-public ov5640-firmware
+  (let ((commit "6e8e591e17e207644dfe747e51026967bb1edab5")
+        (revision "1"))
+    (package
+      (name "ov5640-firmware")
+      (version (git-version "0.0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://megous.com/git/linux-firmware")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "19xmkdvlkczc6zgigy8jdbgnp37i6pc03m2cm3gilvzg8m7v18ad"))))
+      (build-system copy-build-system)
+      (arguments
+       `(#:install-plan '(("ov5640_af.bin" "lib/firmware/"))))
+      (synopsis "Firmware for the OV5640 sensor in the PinePhone")
+      (description "This package provides binary firmware for the 0V5640 sensor
+in the PinePhone.")
+      (home-page "https://megous.com/git/linux-firmware")
+      (license (nonfree (string-append "unknown"))))))
