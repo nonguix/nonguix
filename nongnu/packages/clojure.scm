f93ee0b8b170ac5963c59a3c25ddb7f5ef242aaf
Jelle Licht <jlicht@fsfe.org>
2022-01-13 11:47:28 +0100

    nongnu: Remove clojure-tools.
    This package was upstreamed in 8cf2817d4c2e0.
    
    * nonguix/packages/clojure.scm (clojure-tools): Remove variable.

--- nongnu/packages/clojure.scm
+++ nongnu/packages/clojure.scm
@@ -31,46 +31,6 @@
   #:use-module (nonguix build-system binary)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public clojure-tools
-  (package
-    (name "clojure-tools")
-    (version "1.10.3.1040")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "https://download.clojure.org/install/clojure-tools-"
-                           version
-                           ".tar.gz"))
-       (sha256 (base32 "0xvr9nmk9q789vp32zmmzj4macv8v7y9ivnfd6lf7i8vxgg6hvgv"))))
-    (build-system copy-build-system)
-    (arguments
-     `(#:install-plan
-       '(("deps.edn" "lib/clojure/")
-         ("example-deps.edn" "lib/clojure/")
-         ("tools.edn" "lib/clojure/")
-         ("exec.jar" "lib/clojure/libexec/")
-         (,(string-append "clojure-tools-" version ".jar") "lib/clojure/libexec/")
-         ("clojure" "bin/")
-         ("clj" "bin/"))
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'fix-paths
-           (lambda* (#:key outputs #:allow-other-keys)
-             (substitute* "clojure"
-               (("PREFIX") (string-append (assoc-ref outputs "out") "/lib/clojure")))
-             (substitute* "clj"
-               (("BINDIR") (string-append (assoc-ref outputs "out") "/bin"))
-               (("rlwrap") (which "rlwrap")))
-             #true)))))
-    (inputs `(("rlwrap" ,rlwrap)))
-    (synopsis "CLI tools for the Clojure programming language")
-    (description "The Clojure command line tools can be used to start
-a Clojure repl, use Clojure and Java libraries, and start Clojure
-programs.")
-    (license license:epl1.0)
-    (home-page "https://clojure.org/releases/tools")))
-
-
 ;; This is a hidden package, as it does not really serve a purpose on its own.
 (define leiningen-jar
   (package
