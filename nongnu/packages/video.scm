88a5679b3185c8446efece23fee981f97634ee86
Jelle Licht <jlicht@fsfe.org>
2022-03-07 13:14:32 +0100

    nongnu: Add intel-media-driver.
    * nongnu/packages/video.scm: (intel-media-driver): New variable.

--- nongnu/packages/video.scm
+++ nongnu/packages/video.scm
@@ -49,3 +49,40 @@
 Intel Graphics Compute Runtime for OpenCL and the Intel Media Driver
 for VAAPI.")
     (license license:expat)))
+
+(define-public intel-media-driver
+  (package
+    (name "intel-media-driver")
+    (version "22.2.2")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/intel/media-driver")
+                    (commit (string-append "intel-media-" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1wvx3kvsdq7n5khl0pf7hf1ks4lwqnyjdq8sbjhd7irs3v3prj4j"))))
+    (build-system cmake-build-system)
+    (inputs (list libva gmmlib))
+    (native-inputs (list pkg-config))
+    (arguments
+     '(#:tests? #f ;Tests are run as part of the normal build step
+       #:configure-flags
+       (list "-DENABLE_NONFREE_KERNELS=OFF"
+             (string-append "-DLIBVA_DRIVERS_PATH="
+                            (assoc-ref %outputs "out") "/lib/dri"))))
+    ;; XXX Because of <https://issues.guix.gnu.org/issue/22138>, we need to add
+    ;; this to all VA-API back ends instead of once to libva.
+    (native-search-paths
+     (list (search-path-specification
+            (variable "LIBVA_DRIVERS_PATH")
+            (files '("lib/dri")))))
+    (supported-systems '("i686-linux" "x86_64-linux"))
+    (home-page "https://github.com/intel/media-driver")
+    (synopsis "Intel Media Driver for VAAPI")
+    (description
+     "This package provides a VA-API user mode driver supporting hardware
+accelerated decoding, encoding, and video post processing for the GEN based
+graphics hardware.")
+    (license (list license:expat license:bsd-3))))
