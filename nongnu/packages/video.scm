31e59f0e275b126cdf4a41d18f50d2b5bc4fdad1
Jelle Licht <jlicht@fsfe.org>
2022-03-07 13:13:36 +0100

    nongnu: Add gmmlib.
    * nongnu/packages/video.scm: New file.
    (gmmlib): New variable.

---
+++ nongnu/packages/video.scm
@@ -0,0 +1,51 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2022 Jelle Licht <jlicht@fsfe.org>
+;;;
+;;; This file is not part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (nongnu packages video)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages video)
+  #:use-module (guix build utils)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public gmmlib
+  (package
+    (name "gmmlib")
+    (version "22.0.3")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/intel/gmmlib")
+                    (commit (string-append "intel-gmmlib-" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "07q1rskc6xp17nlwzl2h96pqybcwi8b0nvdgy3n28baai37jayki"))))
+    (build-system cmake-build-system)
+    (arguments
+     ;; Tests are run as part of the normal build step
+     '(#:tests? #f))
+    (home-page "https://github.com/intel/gmmlib")
+    (synopsis "Intel Graphics Memory Management Library")
+    (description
+     "This package provides device specific and buffer management for the
+Intel Graphics Compute Runtime for OpenCL and the Intel Media Driver
+for VAAPI.")
+    (license license:expat)))
