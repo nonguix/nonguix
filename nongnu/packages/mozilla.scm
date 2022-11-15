20fa6065f9cf03a5852aff6417678578389c2ad9
John Kehayias <john.kehayias@protonmail.com>
2022-11-15 14:39:22 -0500

    nongnu: firefox: Update to 107.0 [security fixes].
    Fixes CVE-2022-40674, CVE-2022-45403, CVE-2022-45404, CVE-2022-45405,
    CVE-2022-45406, CVE-2022-45407, CVE-2022-45408, CVE-2022-45409,
    CVE-2022-45410, CVE-2022-45411, CVE-2022-45412, CVE-2022-45413,
    CVE-2022-45415, CVE-2022-45416, CVE-2022-45417, CVE-2022-45418,
    CVE-2022-45419, CVE-2022-45420 and CVE-2022-45421.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 107.0.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -665,20 +665,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20221103000000")
+(define %firefox-build-id "20221115000000")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "106.0.4")
+    (version "107.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "1dwykdd3nyvkv34nq2zwfa6kkzw4w8pw5300y25gfny94ksx06g6"))))
+        (base32 "0jjqarvs5ppyb3395rs4i39cb55li8q8ha9w72zyjmvv75d2wmla"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
