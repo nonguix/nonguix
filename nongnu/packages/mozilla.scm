0afe9f36d8b8b69d3e69fc8c06aeadc1bd95eecd
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2022-11-30 19:35:31 +0100

    nongnu: firefox: Update to 107.0.1.
    * nongnu/packages/mozilla.scm (firefox): Update to 107.0.1.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -665,20 +665,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20221115000000")
+(define %firefox-build-id "20221129000000")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "107.0")
+    (version "107.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "0jjqarvs5ppyb3395rs4i39cb55li8q8ha9w72zyjmvv75d2wmla"))))
+        (base32 "0iq67r9ik6zng9m8zzrsaf1d1fvhpsdpf66whgbb0hwipawm16g2"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
