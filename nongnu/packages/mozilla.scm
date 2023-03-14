440720b7b2ca4789298f3150bc82bf1b5ed4b8c9
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2023-03-14 20:26:14 +0100

    nongnu: firefox: Update to 111.0 [security fixes].
    Fixes CVE-2023-25748, CVE-2023-25749, CVE-2023-25750, CVE-2023-25751,
    CVE-2023-25752, CVE-2023-28159, CVE-2023-28160, CVE-2023-28161,
    CVE-2023-28162, CVE-2023-28163, CVE-2023-28164, CVE-2023-28176 and CVE-2023-28177.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 111.0.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -666,20 +666,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20230228000000")
+(define %firefox-build-id "20230313000000")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "110.0.1")
+    (version "111.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "07s9k34py05sa4nx1dayg394r5wqc5vzas6amcjjd6abd16vg6zi"))))
+        (base32 "0px59syzvypnyrxn7ca8426zrrgiawwl32d6nl7v6zmaf846q071"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
