3a7636d8a5e2b395fe89c128c8501d535f2f3279
Jelle Licht <jlicht@fsfe.org>
2023-05-23 18:20:50 +0200

    nongnu: Add linux@6.3.
    Add linux@6.3 but do not change the default linux so that the build farm has
    time to catch up.
    
    * nongnu/packages/linux.scm (linux-6.3): New variable.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -88,6 +88,9 @@
        "The unmodified Linux kernel, including nonfree blobs, for running Guix System
 on hardware which requires nonfree software to function."))))
 
+(define-public linux-6.3
+  (corrupt-linux linux-libre-6.3))
+
 (define-public linux-6.2
   (corrupt-linux linux-libre-6.2))
 
