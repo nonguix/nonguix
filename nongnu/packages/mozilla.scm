b609a455d603cc752e9248fa4a94a940b7eea4d8
John Kehayias <john.kehayias@protonmail.com>
2023-08-03 11:52:27 -0400

    gnu: firefox: Update to 116.0 [security fixes].
    Fixes CVE-2023-4045, CVE-2023-4046, CVE-2023-4047, CVE-2023-4048,
    CVE-2023-4049, CVE-2023-4050, CVE-2023-4051, CVE-2023-4052, CVE-2023-4053,
    CVE-2023-4054, CVE-2023-4055, CVE-2023-4056, CVE-2023-4057, and CVE-2023-4058.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 116.0.

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -512,20 +512,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to it's release date.
 ;; It's used for cache validation and therefor can lead to strange bugs.
-(define %firefox-build-id "20230711000000")
+(define %firefox-build-id "20230801000000")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "115.0.2")
+    (version "116.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "0m0nxsdsij8sx8xiynar2rivfmz9yd05hzxcjyfvvd0k9h948a3b"))))
+        (base32 "0xms5jwlm7ixd1cf00xpwfj9p3mng8i9ix2jl8kl6x5z8mlhfp91"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
