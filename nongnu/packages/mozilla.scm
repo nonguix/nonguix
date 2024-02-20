c620b749a0a959b2f6a3f20211dbdbf42c53a34a
Tomas Volf <~@wolfsden.cz>
2024-02-20 16:40:42 +0100

    nongnu: firefox: Update to 123.0 [security fixes].
    Fixes CVE-2024-1546, CVE-2024-1547, CVE-2024-1548, CVE-2024-1549,
    CVE-2024-1550, CVE-2024-1551, CVE-2024-1552, CVE-2024-1553,
    CVE-2024-1554, CVE-2024-1555, CVE-2024-1556, CVE-2024-1557.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 123.0.
    
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -532,20 +532,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20240205214308")
+(define %firefox-build-id "20240219104035")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "122.0.1")
+    (version "123.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "1minry6lgf4xvbpdgmly3nm5wwbv3l7lsnd1sgb2zv4ffjd9rw9n"))))
+        (base32 "1dscgn1ymqi4zr29fpj43y8gaspc5wl7a59w657ws56vmnymm24y"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
