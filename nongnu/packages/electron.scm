587cf0a13518c506cd550cdfbf47b3792004cf73
Jonathan Brielmaier <jonathan.brielmaier@web.de>
2024-03-14 23:08:58 +0100

    nongnu: Add electron@29.
    * nongnu/packages/electron.scm (electron-29): New variable.
    
    Co-authored-by: Andre A. Gomes <andremegafone@gmail.com>

--- nongnu/packages/electron.scm
+++ nongnu/packages/electron.scm
@@ -96,3 +96,9 @@ Chromium and is used by the Atom editor and many other apps.")
     (version "28.2.7")
     (source (electron-source version
                              "0sw8nn1jr9x0p4s2g5yp5lrpy8mzq0sjd099x7axbg6cm1c80iff"))))
+(define-public electron-29
+  (package
+    (inherit electron-27)
+    (version "29.1.4")
+    (source (electron-source version
+                             "1d96nhx9j9mzc3q3ald1imqdxdgq9by1qm4q703sjy3knq1p38w3"))))
