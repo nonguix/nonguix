ec3565d2846870834c2edf7bdbc4d434925d927b
John Kehayias <john.kehayias@protonmail.com>
2024-04-08 22:01:54 -0400

    nongnu: linux: Update the default linux package to the 6.8 series.
    Upstream Guix defaulted to linux-libre-6.8 in commit
    40e44bdea6f4c3dc3563427fdf096a60812534a7.  Do likewise here.
    
    * nongnu/packages/linux.scm (linux): Use linux-6.8.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -157,7 +157,7 @@ on hardware which requires nonfree software to function."))))
 (define-public linux-4.19
   (corrupt-linux linux-libre-4.19))
 
-(define-public linux linux-6.7)
+(define-public linux linux-6.8)
 ;; linux-lts points to the *newest* released long-term support version.
 (define-public linux-lts linux-6.1)
 
