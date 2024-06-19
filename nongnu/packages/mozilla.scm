75eccbccc8fb4b63d64cb55a4e309a648f3594a0
Tomas Volf <~@wolfsden.cz>
2024-06-19 19:24:23 +0200

    nongnu: firefox: Update to 127.0.1.
    * nongnu/packages/mozilla.scm (firefox): Update to 127.0.1.
    
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -546,20 +546,20 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20240610130632")
+(define %firefox-build-id "20240618185344")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "127.0")
+    (version "127.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "08a3k7lr6z4gxn5rcjnwpimzcr1w2bm13bzxg0wm1jj6y2ghhsza"))))
+        (base32 "18zayk3yp6zwl5hjcygy9lr1byg1y1lvcqnslzymv5p6dws3zqgq"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
