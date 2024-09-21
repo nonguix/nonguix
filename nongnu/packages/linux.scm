3c3870a9644fc12cd66bfaa5ca0123406124e746
Hilton Chain <hako@ultrarare.space>
2024-09-21 09:16:24 +0800

    nongnu: linux-xanmod: Update to 6.10.11.
    * nongnu/packages/linux.scm (linux-xanmod-version): Update to 6.10.11.
    (linux-xanmod-source): Update hashes.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -276,15 +276,15 @@ distribution with custom settings and new features.  It's built to provide a
 stable, responsive and smooth desktop experience.")))
 
 ;; Linux-XanMod sources
-(define-public linux-xanmod-version "6.9.12")
+(define-public linux-xanmod-version "6.10.11")
 (define-public linux-xanmod-revision "xanmod1")
 (define-public linux-xanmod-source
   (make-linux-xanmod-source
    linux-xanmod-version
    linux-xanmod-revision
    #:xanmod-branch "main"
-   #:kernel-hash (base32 "0jc14s7z2581qgd82lww25p7c4w72scpf49z8ll3wylwk3xh3yi4")
-   #:xanmod-hash (base32 "1n68ylzvcv7sjdlcpfixw79fbinsaywh9svg0v6npdv1afkz95j0")))
+   #:kernel-hash (base32 "09p2z3z8c3aq6ipqdc58x6s52sy0cmyg6mj4f0g5yk755r19hikp")
+   #:xanmod-hash (base32 "0zasbiiwb2wli2w3qqg4fp9gp312g2n85g2qwzyr3fabnqf7h4sh")))
 
 (define-public linux-xanmod-lts-version "6.6.52")
 (define-public linux-xanmod-lts-revision "xanmod1")
