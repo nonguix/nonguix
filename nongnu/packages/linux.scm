e3fb39c68af3f4b0cce011dd5f9f8030c9a1ab6a
Ashish SHUKLA <ashish.is@lostca.se>
2025-01-04 20:17:42 +0100

    nongnu: Remove linux-4.19.
    linux-libre@4.19 was removed from upstream Guix in commit
    7b40b9d2d2ddafd6945f18f19f5e621086d57169 as it is no longer supported
    upstream.
    
    Fixes #361.
    
    * nongnu/packages/linux.scm (linux-4.19): Remove variable.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -152,9 +152,6 @@ on hardware which requires nonfree software to function."))))
 (define-public linux-5.4
   (corrupt-linux linux-libre-5.4))
 
-(define-public linux-4.19
-  (corrupt-linux linux-libre-4.19))
-
 (define-public linux linux-6.11)
 ;; linux-lts points to the *newest* released long-term support version.
 (define-public linux-lts linux-6.6)
