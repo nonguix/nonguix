7c1a45d8e71580fe45f75b4d7eba0a52eb2ed9b0
Tomas Volf <~@wolfsden.cz>
2025-02-04 21:12:39 +0100

    nongnu: firefox: Update to 135.0 [security fixes].
    Fixes CVE-2025-1009, CVE-2025-1010, CVE-2025-1011, CVE-2025-1012,
    CVE-2025-1013, CVE-2025-1014, CVE-2025-1016, CVE-2025-1017,
    CVE-2025-1018, CVE-2025-1019, CVE-2025-1020.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 135.0.
    
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -551,13 +551,13 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20250120192449")
+(define %firefox-build-id "20250203132324")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "134.0.2")
+    (version "135.0")
     (source
      (origin
        (method url-fetch)
@@ -573,7 +573,7 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
                "firefox-esr-compare-paths.patch"
                "firefox-use-system-wide-dir.patch")))
        (sha256
-        (base32 "09yxacfcklgjqbqvcac32llwmlb16d9jhfp2mif9qs7s2gzvfvkc"))))
+        (base32 "0q5r2q6q56kyzl5pknrir9bzlhmzbvv9hi5gi4852izgcali4zl2"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
