513cc8a52a41cbc2c2d5459e1e5478291c0ff090
John Kehayias <john.kehayias@protonmail.com>
2025-02-04 22:05:05 -0500

    nongnu: element-desktop: Update wrapper-plan to include binary path.
    * nongnu/packages/messaging.scm (element-desktop)[arguments]<#:wrapper-plan>:
    Add the path for element-desktop to its wrapper-plan.
    <#:phases>: Remove wrap-where-patchelf-does-not-work phase as it is no longer
    needed.

--- nongnu/packages/messaging.scm
+++ nongnu/packages/messaging.scm
@@ -51,7 +51,7 @@
     (arguments
      (list #:validate-runpath? #f ; TODO: fails on wrapped binary and included other files
            #:wrapper-plan
-           #~'("lib/Element/element-desktop")
+           #~'(("lib/Element/element-desktop" (("out" "/lib/Element"))))
            #:phases
            #~(modify-phases %standard-phases
                (add-after 'binary-unpack 'setup-cwd
@@ -69,15 +69,7 @@
                  (lambda _
                    (mkdir-p (string-append #$output "/bin"))
                    (symlink (string-append #$output "/lib/Element/element-desktop")
-                            (string-append #$output "/bin/element-desktop"))))
-               (add-after 'install-wrapper 'wrap-where-patchelf-does-not-work
-                 (lambda _
-                   (wrap-program (string-append #$output "/lib/Element/element-desktop")
-                     `("LD_LIBRARY_PATH" ":" prefix
-                       (,(string-join
-                          (list
-                           (string-append #$output "/lib/Element"))
-                          ":")))))))))
+                            (string-append #$output "/bin/element-desktop")))))))
     (home-page "https://github.com/vector-im/element-desktop")
     (synopsis "Matrix collaboration client for desktop")
     (description "Element Desktop is a Matrix client for desktop with Element Web at
