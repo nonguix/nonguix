fa416ebdf9e4d5c3b9676ded8829c5875bcf4f0e
Ashish SHUKLA <ashish.is@lostca.se>
2025-03-13 01:57:37 +0100

    nongnu: signal-desktop: Update to 7.46.0.
    * nongnu/packages/messaging.scm (signal-desktop): Update to 7.46.0
    
    Signed-off-by: Jelle Licht <jlicht@fsfe.org>

--- nongnu/packages/messaging.scm
+++ nongnu/packages/messaging.scm
@@ -83,7 +83,7 @@ its core.")
 (define-public signal-desktop
   (package
     (name "signal-desktop")
-    (version "7.45.0")
+    (version "7.46.0")
     (source
      (origin
        (method url-fetch)
@@ -92,7 +92,7 @@ its core.")
          "https://updates.signal.org/desktop/apt/pool/s/" name "/" name "_" version
          "_amd64.deb"))
        (sha256
-        (base32 "1587w27mb968dw36mvww21wqxzwr3nhdcp5wkriwlmz1wv235xxm"))))
+        (base32 "0gnh0jrsgdmb8hw6hl8hwgj0dqkdcnggd9psrrg7ibbihxhh1svf"))))
     (supported-systems '("x86_64-linux"))
     (build-system chromium-binary-build-system)
     (arguments
