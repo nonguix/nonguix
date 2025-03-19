a96e2451bda5aaf9b48339edee392c6a3017d730
Tomas Volf <~@wolfsden.cz>
2025-03-19 17:49:18 +0100

    nongnu: firefox: Update to 136.0.2.
    * nongnu/packages/mozilla.scm (firefox): Update to 136.0.2.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -547,13 +547,13 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20250311003006")
+(define %firefox-build-id "20250317201033")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "136.0.1")
+    (version "136.0.2")
     (source
      (origin
        (method url-fetch)
@@ -569,7 +569,7 @@ MOZ_ENABLE_WAYLAND=1 exec ~a $@\n"
                "firefox-esr-compare-paths.patch"
                "firefox-use-system-wide-dir.patch")))
        (sha256
-        (base32 "1di168z9axxz3s7f248zl1gfjaxjlxvx7f4an7rgfwbjmb7vs4jg"))))
+        (base32 "007jvc6vbn9fvwbl45brww8v2f2jz4ljlz2mds1pfgslhjg7q62k"))))
     (arguments
      (substitute-keyword-arguments (package-arguments firefox-esr)
        ((#:phases phases)
