ae6e9f28343d990f1e11286e99dd195d80dc65b0
Tomas Volf <~@wolfsden.cz>
2025-04-29 21:32:13 +0200

    nongnu: firefox-esr: Update to 128.10.0esr [security fixes].
    Fixes CVE-2025-2817, CVE-2025-4082, CVE-2025-4083, CVE-2025-4084,
    CVE-2025-4087, CVE-2025-4091, CVE-2025-4093.
    
    * nongnu/packages/mozilla.scm (firefox-esr): Update to 128.10.0esr.
    
    Signed-off-by: Hilton Chain <hako@ultrarare.space>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -86,19 +86,19 @@
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-esr-build-id "20250331081530")
+(define %firefox-esr-build-id "20250428115526")
 
 (define-public firefox-esr
   (package
     (name "firefox-esr")
-    (version "128.9.0esr")
+    (version "128.10.0esr")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "01b8rkw1rns282rsgs7rsz5v76mp5wfzhqhb507xvgy0dhf6c12d"))
+        (base32 "1bqw0xmhr6v25lbbqipdcpnvgffvhj5484qdghn3x2qswhk3xn1f"))
        (patches
         (map (lambda (patch)
                (search-path
