26bcc5236c210df0d0fffeb54bddd1c7f859f6e7
Ashish SHUKLA <ashish.is@lostca.se>
2025-08-14 11:15:15 +0200

    nongnu: signal-desktop: Update to 7.66.0.
    * nongnu/packages/messaging.scm (signal-desktop): Update to 7.66.0.
    
    Signed-off-by: Hilton Chain <hako@ultrarare.space>

--- nongnu/packages/messaging.scm
+++ nongnu/packages/messaging.scm
@@ -83,7 +83,7 @@ its core.")
 (define-public signal-desktop
   (package
     (name "signal-desktop")
-    (version "7.65.0")
+    (version "7.66.0")
     (source
      (origin
        (method url-fetch)
@@ -92,7 +92,7 @@ its core.")
          "https://updates.signal.org/desktop/apt/pool/s/" name "/" name "_" version
          "_amd64.deb"))
        (sha256
-        (base32 "1qlcywqm3n2r2mfpifdj9zj2652azvx5hw14cshfhkpvbh0223vn"))))
+        (base32 "0fdgh1cm5crhs8p0a14fzqx83md109gcyrv7sx56n0s5iiz7rblb"))))
     (supported-systems '("x86_64-linux"))
     (build-system chromium-binary-build-system)
     (arguments
