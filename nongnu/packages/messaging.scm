10dd3f67fed7980829188a0ac22334748becaf5a
Ashish SHUKLA <ashish.is@lostca.se>
2025-08-20 21:09:23 +0200

    nongnu: signal-desktop: Update to 7.67.0.
    * nongnu/packages/messaging.scm (signal-desktop): Update to 7.67.0.
    
    Signed-off-by: Jelle Licht <jlicht@fsfe.org>

--- nongnu/packages/messaging.scm
+++ nongnu/packages/messaging.scm
@@ -83,7 +83,7 @@ its core.")
 (define-public signal-desktop
   (package
     (name "signal-desktop")
-    (version "7.66.0")
+    (version "7.67.0")
     (source
      (origin
        (method url-fetch)
@@ -92,7 +92,7 @@ its core.")
          "https://updates.signal.org/desktop/apt/pool/s/" name "/" name "_" version
          "_amd64.deb"))
        (sha256
-        (base32 "0fdgh1cm5crhs8p0a14fzqx83md109gcyrv7sx56n0s5iiz7rblb"))))
+        (base32 "1b8svlb6whlbl62wgia455wbdl847j4v93fvas3vyra2yi18jq2f"))))
     (supported-systems '("x86_64-linux"))
     (build-system chromium-binary-build-system)
     (arguments
