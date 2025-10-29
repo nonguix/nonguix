e6596d523cdadcec65307371163fbea850de517b
Tomas Volf <~@wolfsden.cz>
2025-10-29 19:37:42 +0100

    nongnu: wasm32-wasi-clang: Update to 17.0.6.
    * nongnu/packages/wasm.scm (wasm32-wasi-clang): Base on llvm-17.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/wasm.scm
+++ nongnu/packages/wasm.scm
@@ -108,7 +108,7 @@ other APIs.")
 (define llvm-monorepo (@@ (gnu packages llvm) llvm-monorepo))
 
 (define-public wasm32-wasi-clang
-  (let ((base (clang-from-llvm llvm-15 wasm32-wasi-clang-runtime)))
+  (let ((base (clang-from-llvm llvm-17 wasm32-wasi-clang-runtime)))
     (package (inherit base)
       (name "wasm32-wasi-clang")
       (inputs
