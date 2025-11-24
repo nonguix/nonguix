5fde74e0c5e5a289d9b6f058459c96162fccf375
Tomas Volf <~@wolfsden.cz>
2025-11-24 22:29:10 +0100

    nongnu: firefox: Update to 145.0.1.
    * nongnu/packages/mozilla.scm (firefox): Update to 145.0.1.
    
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -529,20 +529,20 @@ Release (ESR) version.")
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20251110115236")
+(define %firefox-build-id "20251118130425")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "145.0")
+    (version "145.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "174c1fhwymzd1pdvcm5hf4vx541vxprcpqi5qx2x6all1vdjh27b"))
+        (base32 "0mzlkymgp1dcyjlv30xaq96f0ls0mxn25264xygidw2g62yib0dl"))
        (patches
         (map (lambda (patch)
                (search-path
