1ae5ee1e61b8f4e41c93e69a0303aeaeddb39b1e
Natalie Atkinson <natalie.atkinson95@pm.me>
2026-01-23 09:16:54 +1100

    nongnu: Add kustomize
    Signed-off-by: Jelle Licht <jlicht@fsfe.org>

--- nongnu/packages/k8s.scm
+++ nongnu/packages/k8s.scm
@@ -145,6 +145,42 @@ view logs.")
 such as Kubernetes (or OpenShift).")
     (license license:expat)))
 
+(define-public kustomize
+  (package
+    (name "kustomize")
+    (version "5.8.0")
+    (source (origin
+              (method url-fetch)
+              (uri
+               (string-append
+                "https://github.com/kubernetes-sigs/kustomize"
+                "/releases/download/kustomize%2Fv"
+                version "/kustomize_v" version "_linux_amd64.tar.gz"))
+              (sha256
+               (base32
+                "1vy504gr9624yzws73ry8f9mp9k6cxcxbcfjli52incd6l3q7yjd"))))
+    (build-system copy-build-system)
+    (arguments
+     (list
+      #:substitutable? #f
+      #:install-plan
+      #~'(("kustomize" "bin/"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'install 'chmod
+            (lambda _
+              (chmod "kustomize" #o555))))))
+    (home-page "https://github.com/kubernetes-sigs/kustomize")
+    (supported-systems '("x86_64-linux"))
+    (synopsis "Customization of kubernetes YAML configuration")
+    (description
+     "Kustomize is a template-free configuration management tool for
+Kubernetes that allows you to customize application manifests without
+modifying the original YAML files.  It uses overlays and patches to manage
+configuration variations across different environments while maintaining a
+common base configuration.")
+    (license license:asl2.0)))
+
 (define-public helm-kubernetes
   (package
     (name "helm-kubernetes")
