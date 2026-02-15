3be971913c3d0c0dc8021e2e1cd25f1050acf6be
John Kehayias <john.kehayias@protonmail.com>
2026-02-14 21:27:44 -0500

    nongnu: linux: Update to 6.18.
    This follows Guix's change to the default kernel.
    
    * nongnu/packages/linux.scm (linux): Use linux-6.18.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -202,7 +202,7 @@ on hardware which requires nonfree software to function."))))
 (define-public linux-5.4
   (corrupt-linux linux-libre-5.4))
 
-(define-public linux linux-6.17)
+(define-public linux linux-6.18)
 ;; linux-lts points to the *newest* released long-term support version.
 (define-public linux-lts linux-6.12)
 
