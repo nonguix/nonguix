2cb40420f0cd37d9fa24c57b0d8532b815318815
Ada Stevenson <adanskana@gmail.com>
2026-03-26 00:14:43 +0800

    nongnu: sof-firmware: Update to 2025.12.2.
    * nongnu/packages/linux.scm (sof-firmware): Update to 2025.12.2.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -1520,7 +1520,7 @@ documented in the respective processor revision guides.")
 (define-public sof-firmware
   (package
     (name "sof-firmware")
-    (version "2025.05")
+    (version "2025.12.2")
     (source
      (origin
        (method url-fetch)
@@ -1528,7 +1528,7 @@ documented in the respective processor revision guides.")
                            version "/sof-bin-" version ".tar.gz"))
        (sha256
         (base32
-         "1k3bm2w5r49nl995jh0zjas4sjzqzdiii43j2vdwpirq1lxn1wp2"))))
+         "05zmg7n67xbc49whjz3q43zq79jzcxxnb0m70p70jk6rlvin6gsk"))))
     (build-system copy-build-system)
     (arguments
      `(#:install-plan
