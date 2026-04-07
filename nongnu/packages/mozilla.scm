ca71036e9356f2b94bac2e6321a9338b18dcc2b8
Tomas Volf <~@wolfsden.cz>
2026-04-07 17:36:33 +0200

    nongnu: firefox: Update to 149.0.2 [security fixes].
    Fixes CVE-2026-5731, CVE-2026-5732, CVE-2026-5733, CVE-2026-5734,
    CVE-2026-5735.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 149.0.2.
    
    Signed-off-by: Hilton Chain <hako@ultrarare.space>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -526,20 +526,20 @@ Release (ESR) version.")
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20260323124224")
+(define %firefox-build-id "20260406133250")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "149.0")
+    (version "149.0.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "12jb930qhqhpzjpn5smms7g4n6y40zvxcrg8w42696wxk7pgsqdq"))
+        (base32 "1kkhg3lgyakinn5ankxrlxbf39wi61mqbhwrj1l4q5l7y0n1m4va"))
        (patches
         (nongnu-patches
          "firefox-restore-desktop-files.patch"
