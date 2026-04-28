7e2c3063e508cbec140680702b69f914f6c37534
Tomas Volf <~@wolfsden.cz>
2026-04-28 21:01:25 +0200

    nongnu: firefox-esr: Update to 140.10.1esr [security fixes].
    Fixes CVE-2026-6746, CVE-2026-6747, CVE-2026-6748, CVE-2026-6749,
    CVE-2026-6750, CVE-2026-6751, CVE-2026-6752, CVE-2026-6753,
    CVE-2026-6754, CVE-2026-6757, CVE-2026-6759, CVE-2026-6761,
    CVE-2026-6762, CVE-2026-6763, CVE-2026-6764, CVE-2026-6765,
    CVE-2026-6766, CVE-2026-6767, CVE-2026-6769, CVE-2026-6770,
    CVE-2026-6771, CVE-2026-6772, CVE-2026-6776, CVE-2026-6785,
    CVE-2026-6786, CVE-2026-7320, CVE-2026-7321, CVE-2026-7322,
    CVE-2026-7323.
    
    * nongnu/packages/mozilla.scm (firefox-esr): Update to 140.10.1esr.
    
    Signed-off-by: Hilton Chain <hako@ultrarare.space>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -88,19 +88,19 @@
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-esr-build-id "20260407093607")
+(define %firefox-esr-build-id "20260428112555")
 
 (define-public firefox-esr
   (package
     (name "firefox-esr")
-    (version "140.9.1esr")
+    (version "140.10.1esr")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "1hld2kbzvrmr7pqq0r5hw639xl8kw29lm2hsbn0m4kxang1fdlj5"))
+        (base32 "1fx52y52c3dmgr5gm64x40r2n6zq5ckqiqsd6vlhsly5wb1w0xaf"))
        (patches
         (nongnu-patches
          "firefox-restore-desktop-files.patch"
