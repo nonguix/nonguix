4ae06fb5cb75f2ca6b0f2f384f41677ae28c069a
Tomas Volf <~@wolfsden.cz>
2026-06-03 00:04:38 +0200

    nongnu: firefox: Update to 151.0.3 [security fixes].
    Fixes CVE-2026-10701, CVE-2026-10702.
    
    * nongnu/packages/mozilla.scm (firefox): Update to 151.0.3.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -526,20 +526,20 @@ Release (ESR) version.")
 
 ;; Update this id with every firefox update to its release date.
 ;; It's used for cache validation and therefore can lead to strange bugs.
-(define %firefox-build-id "20260525185324")
+(define %firefox-build-id "20260601182653")
 
 (define-public firefox
   (package
     (inherit firefox-esr)
     (name "firefox")
-    (version "151.0.2")
+    (version "151.0.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://archive.mozilla.org/pub/firefox/releases/"
                            version "/source/firefox-" version ".source.tar.xz"))
        (sha256
-        (base32 "1j7q00fwdijvg5b01jmadhqabwmwdcqd15srmkbqsqgjk5vjdi33"))
+        (base32 "0rpy7ds707jzav9jaragsmgrdqc75niclp3ia0vpl1c9si72h83q"))
        (patches
         (nongnu-patches
          "firefox-restore-desktop-files.patch"
