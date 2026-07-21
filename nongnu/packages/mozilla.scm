90b511402d9d862283dbe8dddc2df8f3e618f650
Tomas Volf <~@wolfsden.cz>
2026-07-21 23:55:19 +0200

    nongnu: Remove firefox-esr/wayland.
    * nongnu/packages/mozilla.scm (firefox-esr/wayland): Delete variable.
    
    Signed-off-by: Jonathan Brielmaier <jonathan.brielmaier@web.de>

--- nongnu/packages/mozilla.scm
+++ nongnu/packages/mozilla.scm
@@ -567,7 +567,5 @@ the official icon and the name \"firefox\".")))
 
 ;; As of Firefox 121.0, Firefox uses Wayland by default. This means we no
 ;; longer need a seperate package for Firefox on Wayland.
-(define-public firefox-esr/wayland
-  (deprecated-package "firefox-esr-wayland" firefox-esr))
 (define-public firefox-wayland
   (deprecated-package "firefox-wayland" firefox))
