585c431a4ff4b7aca053841cb1777b59e45c6c38
John Kehayias <john.kehayias@protonmail.com>
2026-08-22 01:09:19 -0400

    nongnu: Remove linux-7.0.
    Upstream has dropped this version.
    
    * nongnu/packages/linux.scm (linux-7.0): Delete variable.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -188,9 +188,6 @@ on hardware which requires nonfree software to function."))))
 (define-public linux-7.1
   (corrupt-linux linux-libre-7.1))
 
-(define-public linux-7.0
-  (corrupt-linux linux-libre-7.0))
-
 (define-public linux-6.18
   (corrupt-linux linux-libre-6.18))
 
