70818366e6145ff406664f3931041243ae5779a1
Jelle Licht <jlicht@fsfe.org>
2026-09-15 23:21:07 +0200

    nongnu: linux: Update to 7.2.
    This follows Guix's change to the default kernel.
    
    * nongnu/packages/linux.scm (linux): Use linux-7.2.

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -206,7 +206,7 @@ on hardware which requires nonfree software to function."))))
 (define-public linux-6.1
   (corrupt-linux linux-libre-6.1))
 
-(define-public linux linux-7.1)
+(define-public linux linux-7.2)
 ;; linux-lts points to the *newest* released long-term support version.
 (define-public linux-lts linux-6.18)
 
